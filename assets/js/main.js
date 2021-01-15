74682b227e1e2972b8fd6eb777f5e68c01192a90
Great Ihevueme <ihevuemeg@gmail.com>
2021-01-15 09:08:22 +0100

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -252,3 +252,33 @@
     aos_init();
   });
 })(jQuery);
+
+/* =========================================
+              Clients
+============================================ */
+$(function () {
+  $("#clients-list").owlCarousel({
+      items: 6,
+      autoplay: false,
+      smartSpeed: 700,
+      loop: true,
+      autoplayHoverPause: true,
+      nav: true,
+      dots: false,
+      navText: ['<i class="fa fa-angle-left font"></i>', '<i class="fa fa-angle-right font"></i>'],
+      responsive: {
+          // breakpoint from 0 up
+          0: {
+              items: 2
+          },
+          // breakpoint from 480 up
+          480: {
+              items: 3
+          },
+          // breakpoint from 768 up
+          768: {
+              items: 6
+          }
+      }
+  });
+});
